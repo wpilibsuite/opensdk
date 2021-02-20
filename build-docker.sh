b12708a16b93a97bd5aca907e04074a619d8028c
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-19 21:45:23 -0600

    Simplify docker setup

--- build-docker.sh
+++ build-docker.sh
@@ -2,19 +2,15 @@
 
 TAGS=( "14.04" "18.04" )
 
-if ! SKIP_IMG_BUILD; then
-    for tag in "${TAGS[@]}"; do
-        docker build \
-            -t "wpilib/toolchain-builder:$tag" \
-            --build-arg BASE="$tag" .
-    done
-fi
-
 if ! [[ " ${TAGS[@]} " =~ " $1 " ]]; then
     echo "Tag not found. Try one of these..."
     echo "${TAGS[@]}"
     exit 1
 fi
+docker build \
+    -t "wpilib/toolchain-builder:$1" \
+    --build-arg BASE="$tag" .
+
 
 [ "$CI" != true ] && DOCKER_ARG="-it"
 

5c7091ac247faacee035dc656d60de7e3277b0ae
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-23 14:01:35 -0600

    MacOS use -fPIC
    MacOS needs to use position independend code

--- scripts/macos_setup.sh
+++ scripts/macos_setup.sh
@@ -7,8 +7,9 @@ $IS_MAC && PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 $IS_MAC && PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 
 # Clang Specific
-$IS_MAC && CFLAGS="-fbracket-depth=512"
-$IS_MAC && CXXFLAGS="-fbracket-depth=512"
+# Clang on MacOS appears to fail without these
+$IS_MAC && CFLAGS="-fbracket-depth=512 -fPIC"
+$IS_MAC && CXXFLAGS="-fbracket-depth=512 -fPIC"
 
 unset IS_MAC
 export PATH CFLAGS CXXFLAGS

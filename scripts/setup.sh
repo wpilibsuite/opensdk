0a7ce8ff7be0d5f6f7bb59957bef4994ad8100e2
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-23 18:58:37 -0600

    Verbose setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,15 @@
 SETUP_DIR="$ROOT_DIR/scripts/setup"
 
 source "$SETUP_DIR/tools.sh"
+echo "Enabling coreutils"
 source "$SETUP_DIR/coreutils.sh"
+echo "Processing Build Args"
 source "$SETUP_DIR/process_args.sh"
+echo "Applying MacOS Specifc Flags"
 source "$SETUP_DIR/macos.sh"
+echo "Applying Linux/Windows Specifc Flags"
 source "$SETUP_DIR/linux.sh" # Includes windows
+echo "Applying Build Info"
 source "$SETUP_DIR/collect_build_info.sh"
 bash "$ROOT_DIR/scripts/confirm_gnu.sh" || exit
 

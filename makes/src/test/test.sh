ff85bfd7bca245f2458c572b4733a2daa4d5d6ce
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-23 22:58:43 -0600

    Remove aarch64-bionic-linux-gnu for the meantime

--- makes/src/test/test.sh
+++ makes/src/test/test.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
 
-set -e -x
-
 cd "$(dirname "$0")"
 
-"$CC" ./main.c -o a.out
-file a.out
-rm a.out
+FAIL=true
+
+"$CC" ./main.c -o a.out && {
+    file a.out
+    rm a.out
+    FAIL=false
+} > /dev/null
+
+if $FAIL; then
+    "$CC Failed to build"
+    exit 1
+fi

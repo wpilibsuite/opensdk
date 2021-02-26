9cd5728d33d40fc4881670566d0cba678ca75101
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-26 14:09:01 -0600

    Changes after testing on a real mac

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -5,15 +5,15 @@ is-mac && PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 export PATH
 
 # Clang appears to have issues
-# GCC_VER=10
-is-mac && CC="clang"
-is-mac && CXX="clang++"
-is-mac && CPP="clang++"
-#is-mac && LD="gcc-${GCC_VER}"
-# unset GCC_VER
-export CC CXX CPP # LD
+GCC_VER=10
+is-mac && CC="gcc-$GCC_VER"
+is-mac && CXX="g++-$GCC_VER"
+is-mac && CPP="cpp-$GCC_VER"
+is-mac && LD="ld"
+unset GCC_VER
+export CC CXX CPP LD
 
 # MacOS Flags
-is-mac && CFLAGS="-fbracket-depth=512 -fPIC"
-is-mac && CXXFLAGS="-fbracket-depth=512 -fPIC"
+is-mac && CFLAGS="-fPIC"
+is-mac && CXXFLAGS="-fPIC"
 export CFLAGS CXXFLAGS

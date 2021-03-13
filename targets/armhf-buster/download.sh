961a051cd861efa3838b49b9eb9a5e388c432e21
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-12 21:54:02 -0600

    Support Raspberry Pi OS 64-bit as arm64-buster

--- targets/armhf-buster/download.sh
+++ targets/armhf-buster/download.sh
@@ -5,23 +5,17 @@ source "$(dirname "$0")/../../scripts/downloads_tools.sh" || exit
 
 signed sig https://ftp.gnu.org/gnu/gcc/gcc-${V_GCC}/gcc-${V_GCC}.tar.gz
 signed sig https://ftp.gnu.org/gnu/binutils/binutils-${V_BIN}.tar.bz2
+signed sig https://ftp.gnu.org/gnu/gdb/gdb-${V_GDB}.tar.gz
 
-basic-download https://ftp.gnu.org/gnu/gdb/gdb-${V_GDB}.tar.gz
 POOL="http://archive.raspbian.org/raspbian/pool/main/"
 basic-download $POOL/g/gcc-8/libgcc1_${Va_LIBGCC}_armhf.deb
 basic-download $POOL/g/gcc-8/libgcc-8-dev_${Va_LIBGCC}_armhf.deb
 basic-download $POOL/g/gcc-8/libatomic1_${Va_LIBSTDCPP}_armhf.deb
-basic-download $POOL/g/gcc-8/libatomic1-dbg_${Va_LIBSTDCPP}_armhf.deb
 basic-download $POOL/g/gcc-8/libstdc++6_${Va_LIBSTDCPP}_armhf.deb
-basic-download $POOL/g/gcc-8/libstdc++6-8-dbg_${Va_LIBSTDCPP}_armhf.deb
 basic-download $POOL/g/gcc-8/libstdc++-8-dev_${Va_LIBSTDCPP}_armhf.deb
 basic-download $POOL/g/gcc-8/libasan5_${Va_LIBSTDCPP}_armhf.deb
-basic-download $POOL/g/gcc-8/libasan5-dbg_${Va_LIBSTDCPP}_armhf.deb
 basic-download $POOL/g/gcc-8/libgomp1_${Va_LIBSTDCPP}_armhf.deb
-basic-download $POOL/g/gcc-8/libgomp1-dbg_${Va_LIBSTDCPP}_armhf.deb
 basic-download $POOL/g/gcc-8/libubsan1_${Va_LIBSTDCPP}_armhf.deb
-basic-download $POOL/g/gcc-8/libubsan1-dbg_${Va_LIBSTDCPP}_armhf.deb
 basic-download $POOL/g/glibc/libc6_${Va_LIBC}_armhf.deb
-basic-download $POOL/g/glibc/libc6-dbg_${Va_LIBC}_armhf.deb
 basic-download $POOL/g/glibc/libc6-dev_${Va_LIBC}_armhf.deb
 basic-download $POOL/l/linux/linux-libc-dev_${Va_LINUX}_armhf.deb

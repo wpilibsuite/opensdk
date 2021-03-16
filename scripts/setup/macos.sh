e34e511e1ac4fb5ea9cc1ac35360ffa3b9e3bd3c
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-15 22:28:51 -0500

    MacOS try to use target for M1 builds

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -2,18 +2,19 @@
 
 is-mac && PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 is-mac && PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+is-mac && PATH="/Applications/Xcode_12.4.app/Contents/Developer/usr/bin:$PATH"
 export PATH
 
-# Clang appears to have issues
-GCC_VER=10
 is-mac && CC="clang" # XCode
 is-mac && CXX="clang++" # XCode
 # is-mac && CPP="cpp-$GCC_VER"
 is-mac && LD="ld" # Apple linker
-unset GCC_VER
 export CC CXX CPP LD
 
+
 # MacOS Flags
-is-mac && CFLAGS="-fcommon"
-is-mac && CXXFLAGS="-fcommon"
+is-mac && CFLAGS="-fcommon -target ${WPIHOSTTARGET}"
+is-mac && CXXFLAGS="-fcommon -target ${WPIHOSTTARGET}"
+is-mac && LDFLAGS="-target ${WPIHOSTTARGET}"
+
 export CFLAGS CXXFLAGS

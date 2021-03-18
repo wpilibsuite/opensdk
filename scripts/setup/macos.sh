ed8d6eded52216c197abcb8941ee0ad8b002fa3f
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-18 13:34:35 -0500

    Retry Universal Binary

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -6,23 +6,28 @@ PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 export PATH
 
-CC="cc" # XCode
-CXX="c++" # XCode
-AR="ar" # Xcode
-LD="ld" # Xcode
-export CC CXX AR LD
-
+SDK_BINS="/Library/Developer/CommandLineTools/usr/bin"
 SDK_PATH="/Library/Developer/CommandLineTools/SDKs"
+
+# override autotools program prefix as this is LLVM
+CC="$SDK_BINS/cc"
+CXX="$SDK_BINS/c++"
+AR="$SDK_BINS/ar"
+LD="$SDK_BINS/ld"
+NM="$SDK_BINS/nm"
+RANLIB="$SDK_BINS/ranlib"
+LIPO="$SDK_BINS/lipo"
+OBJDUMP="$SDK_BINS/objdump"
+export CC CXX AR LD NM RANLIB LIPO OBJDUMP
+
 ls -l "$SDK_PATH"
+[ -d "$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk" ] || exit
 
 # MacOS Flags
 CPPFLAGS="-isysroot $SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk -mmacosx-version-min=${WPI_HOST_SDK_MIN}"
 CFLAGS="$CPPFLAGS -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
 CXXFLAGS="$CPPFLAGS -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
-
-# The following was a test for universal binary
-# CFLAGS="$CPPFLAGS -fcommon -arch arm64 -arch x86_64"
-# CXXFLAGS="$CPPFLAGS -fcommon -arch arm64 -arch x86_64"
+unset SDK_BINS
 unset SDK_PATH
 
 export CPPFLAGS CFLAGS CXXFLAGS

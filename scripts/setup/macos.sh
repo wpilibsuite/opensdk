4c9c6ba4bd2ff008a4495f0716c60e44d4d7a3c3
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-18 15:20:40 -0500

    Builds without GDB

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -9,25 +9,39 @@ export PATH
 SDK_BINS="/Library/Developer/CommandLineTools/usr/bin"
 SDK_PATH="/Library/Developer/CommandLineTools/SDKs"
 
-# override autotools program prefix as this is LLVM
-CC="$SDK_BINS/cc"
-CXX="$SDK_BINS/c++"
-AR="$SDK_BINS/ar"
-LD="$SDK_BINS/ld"
-NM="$SDK_BINS/nm"
-RANLIB="$SDK_BINS/ranlib"
-LIPO="$SDK_BINS/lipo"
-OBJDUMP="$SDK_BINS/objdump"
+CC="cc"
+CXX="c++"
+AR="ar"
+LD="ld"
+NM="nm"
+RANLIB="ranlib"
+LIPO="lipo"
+OBJDUMP="objdump"
 export CC CXX AR LD NM RANLIB LIPO OBJDUMP
 
 ls -l "$SDK_PATH"
 [ -d "$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk" ] || exit
 
 # MacOS Flags
-CPPFLAGS="-isysroot $SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk -mmacosx-version-min=${WPI_HOST_SDK_MIN}"
-CFLAGS="$CPPFLAGS -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
-CXXFLAGS="$CPPFLAGS -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
+CPPFLAGS=( # do this so we can maintain with arrays
+    "-isysroot $SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk"
+    "-I$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk/usr/include/"
+    "-mmacosx-version-min=${WPI_HOST_SDK_MIN}"
+)
+CFLAGS=(
+    "${CPPFLAGS[@]}"
+    "-fcommon"
+    "$WPI_HOST_CPP_FLAGS_APPEND"
+)
+CXXFLAGS=(
+    "${CPPFLAGS[@]}"
+    "-fcommon"
+    "$WPI_HOST_CPP_FLAGS_APPEND"
+)
 unset SDK_BINS
 unset SDK_PATH
 
+CFLAGS="$(echo "${CFLAGS[@]}")"
+CXXFLAGS="$(echo "${CXXFLAGS[@]}")"
+CPPFLAGS="$(echo "${CPPFLAGS[@]}")"
 export CPPFLAGS CFLAGS CXXFLAGS

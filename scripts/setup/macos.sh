843fe566aaa2bbe4574728b57db8b0f1ebf713c9
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-18 15:39:37 -0500

    Remove newline

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -22,9 +22,9 @@ ls -l "$SDK_PATH"
 [ -d "$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk" ] || exit
 
 # MacOS Flags
-CPPFLAGS="-isysroot $SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk
-    -I$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk/usr/include/
-    -mmacosx-version-min=${WPI_HOST_SDK_MIN}"
+CPPFLAGS="-isysroot $SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk"
+CPPFLAGS+=" -I$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk/usr/include/"
+CPPFLAGS+=" -mmacosx-version-min=${WPI_HOST_SDK_MIN}"
 CFLAGS="${CPPFLAGS} -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
 CXXFLAGS="${CPPFLAGS} -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
 

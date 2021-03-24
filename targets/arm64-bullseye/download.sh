bdb2d55e01c3ed1d50a91252b7e43c020a874ca6
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-23 20:48:34 -0500

    Add libgcc-s1 as a dependency to bullseye

--- targets/arm64-bullseye/download.sh
+++ targets/arm64-bullseye/download.sh
@@ -5,6 +5,7 @@ source "${SCRIPT_DIR}/downloads_tools.sh" || exit
 
 signed sig https://ftp.gnu.org/gnu/gcc/gcc-${V_GCC}/gcc-${V_GCC}.tar.gz
 
+package-debian g/gcc-10/libgcc-s1_${Va_LIBGCC}_arm64.deb
 package-debian g/gcc-10/libgcc-10-dev_${Va_LIBGCC}_arm64.deb
 package-debian g/gcc-10/libasan6_${Va_LIBGCC}_arm64.deb
 package-debian g/gcc-10/libatomic1_${Va_LIBGCC}_arm64.deb

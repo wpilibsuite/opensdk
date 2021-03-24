7c9774673ae64cefe9b07673f1a96e12f1242290
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-23 22:46:01 -0500

    Fix windows build

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ source "$ROOT_DIR/scripts/setup.sh"
 if [ "${WPITARGET}" = "Windows" ]; then
     # Recursivly build to setup host to help the canadian build
     STOP_AT_GCC=true bash \
-        "$0" "hosts/linux_x86_64.env" "$2" || exit
+        "$0" "hosts/linux_x86_64.env" "$2" "$3" || exit
 fi
 
 # Prep builds
@@ -20,14 +20,17 @@ bash "${TARGET_CFG}/download.sh" || exit
 bash "${TARGET_CFG}/repack.sh" "${REPACK_DIR}/" || exit
 popd
 
-bash "${ROOT_DIR}/scripts/target_utils.sh"
-
 mkdir -p "${BUILD_DIR}"
 MAKE="make -C ${ROOT_DIR}/makes/ M=${BUILD_DIR}"
-if [ "$WPITARGET" != "sysroot" ]; then
-    ${MAKE} basic
-else
+if [ "$WPITARGET" = "sysroot" ]; then
     ${MAKE} sysroot
+else
+    bash "${ROOT_DIR}/scripts/target_utils.sh"
+
+    ${MAKE} basic
+    if [ "$STOP_AT_GCC" = "true" ]; then
+        exit 0
+    fi
 fi
 
 # Package build for release

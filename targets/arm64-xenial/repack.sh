7af6ed8e45d334b6f511e112dc383db730f1bdf5
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-23 19:39:16 -0500

    Use gcc 5.5.0 on 5.4.0 sysroot to potentially fix macos build.
    may be unstable.

--- targets/arm64-xenial/repack.sh
+++ targets/arm64-xenial/repack.sh
@@ -7,4 +7,19 @@ source "$SCRIPT_DIR/../../scripts/repack_tools.sh" || exit
 
 REPACK_DIR="$1"
 
+# redefine as there are xenial quirks
+# that need to be addressed.
+function fix-headers() {
+    FULL_VER="${1}"
+    MAJOR_VER="${FULL_VER/\.*/}"
+
+    mv "${REPACK_DIR}"/usr/lib/gcc/${TARGET_TUPLE}/{${MAJOR_VER},${FULL_VER}} || true
+    rm "${REPACK_DIR}"/usr/include/${TARGET_TUPLE}/c++/${FULL_VER} || true
+    rm "${REPACK_DIR}"/usr/include/${TARGET_TUPLE}/c++/${V_GCC_SYSROOT} || true
+    mv "${REPACK_DIR}"/usr/include/${TARGET_TUPLE}/c++/{${MAJOR_VER},${FULL_VER}} || true
+    rm "${REPACK_DIR}"/usr/include/c++/${FULL_VER} || true
+    rm "${REPACK_DIR}"/usr/include/c++/${V_GCC_SYSROOT} || true
+    mv "${REPACK_DIR}"/usr/include/c++/{${MAJOR_VER},${FULL_VER}} || true
+}
+
 repack-debian "$REPACK_DIR" "$DOWNLOAD_DIR" "$TARGET_TUPLE" "$V_GCC"

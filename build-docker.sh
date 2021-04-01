8a93ed1291566ebdb775183285c29ab8e7073b51
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-03-31 21:25:19 -0500

    Move docker image build into script

--- build-docker.sh
+++ build-docker.sh
@@ -1,27 +1,12 @@
 #! /usr/bin/env bash
 
-TAGS=( "alpine" "14.04" "18.04" )
+TAG="$1" bash res/build.sh
 
-if ! [[ " ${TAGS[@]} " =~ " $1 " ]]; then
-    echo "Tag not found. Try one of these..."
-    echo "${TAGS[@]}"
-    exit 1
+if [ "$CI" != true ]; then
+    DOCKER_ARG="-it"
 fi
 
-FILE=ubuntu
-[ "$1" = alpine ] && FILE=alpine
-
-pushd "res"
-docker build \
-    --build-arg BASE="$1" \
-    -t "wpilib/toolchain-builder:$1" \
-    -f "Dockerfile.$FILE" \
-    . || exit
-popd
-
-[ "$CI" != true ] && DOCKER_ARG="-it"
-
 docker run \
-   --rm $DOCKER_ARG -v "${PWD}:/work" \
-   "wpilib/toolchain-builder:$1" \
-   bash -c "cd /work; ./build.sh '$2' '$3' '$4'"
+    --rm $DOCKER_ARG -v "${PWD}:/work" \
+    "wpilib/toolchain-builder:$1" \
+    bash -c "cd /work; ./build.sh '$2' '$3' '$4'"
---
+++ res/build.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+TAGS=( "alpine" "14.04" "18.04" )
+BIN_PWD="$(dirname "$0")"
+
+if ! [[ " ${TAGS[@]} " =~ " $TAG " ]]; then
+    echo "Tag not found. Try one of these..."
+    echo "${TAGS[@]}"
+    exit 1
+fi
+
+DOCKERFILE=Dockerfile
+[[ "$TAG" =~ *"alpine" ]] && DOCKERFILE=Dockerfile.alpine
+
+pushd "$BIN_PWD"
+docker build \
+    --build-arg BASE="$TAG" \
+    -t "wpilib/toolchain-builder:$TAG" \
+    -f "$DOCKERFILE" \
+    . || exit
+popd

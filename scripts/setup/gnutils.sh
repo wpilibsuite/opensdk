9b58c13b19fa2a04ff166891aa8a014026b8509f
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-09-07 14:41:27 -0500

    Set macOS paths from homebrew on a arch based use case

--- scripts/setup/gnutils.sh
+++ scripts/setup/gnutils.sh
@@ -1,9 +1,21 @@
 #! /usr/bin/env bash
 
-is-mac && PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-is-mac && PATH="/usr/local/opt/binutils/libexec/gnubin:$PATH"
-is-mac && PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
-is-mac && PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-is-mac && PATH="/opt/homebrew/opt/binutils/libexec/gnubin:$PATH"
-is-mac && PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH
+if is-mac; then
+    case "$(uname -m)" in
+    x86_64)
+        PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+        PATH="/usr/local/opt/binutils/libexec/gnubin:$PATH"
+        PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+        ;;
+    arm64)
+        PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+        PATH="/opt/homebrew/opt/binutils/libexec/gnubin:$PATH"
+        PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
+        ;;
+    *)
+        echo "[FATAL] Unsupported macOS build system."
+        exit 1
+        ;;
+    esac
+    export PATH
+fi

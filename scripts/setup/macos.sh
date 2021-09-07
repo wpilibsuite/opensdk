f076ab4db3a203a04e99c51f2c4fd2292b38d2f2
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-09-07 14:29:33 -0500

    Use latest stable XCode SDK release.
    Minimum macOS target version is still 10.10

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -2,18 +2,14 @@
 
 is-mac || return 0
 
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH
-
 SDK_PATH="/Library/Developer/CommandLineTools/SDKs"
 
 ls -l "$SDK_PATH"
-[ -d "$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk" ] || exit
+[ -d "$SDK_PATH/MacOSX.sdk" ] || exit
 
 MacOS Flags
-CPPFLAGS="-isysroot $SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk"
-CPPFLAGS+=" -I$SDK_PATH/MacOSX${WPI_HOST_SDK_CUR}.sdk/usr/include/"
+CPPFLAGS="-isysroot $SDK_PATH/MacOSX.sdk"
+CPPFLAGS+=" -I$SDK_PATH/MacOSX.sdk/usr/include/"
 CPPFLAGS+=" -mmacosx-version-min=${WPI_HOST_SDK_MIN}"
 CFLAGS="${CPPFLAGS} -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
 CXXFLAGS="${CPPFLAGS} -fcommon $WPI_HOST_CPP_FLAGS_APPEND"

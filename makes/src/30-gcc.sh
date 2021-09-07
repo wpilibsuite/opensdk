9110a9bc8a373e329e3aef8424db5cbbf98ec115
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-09-07 18:15:37 -0500

    Configure gcc with flags from distro maintainers

--- makes/src/30-gcc.sh
+++ makes/src/30-gcc.sh
@@ -3,32 +3,84 @@
 # shellcheck source=./common.sh
 source "$(dirname "$0")/common.sh"
 
-if [ "${WPI_HOST_NAME}" != "Windows" ]; then
-    # Only on GCC for the time being
-    CONFIGURE_COMMON[${#CONFIGURE_COMMON[@]}]="--with-system-zlib"
-fi
-
 rm -rf "${BUILD_DIR}/gcc-build"
 mkdir "${BUILD_DIR}/gcc-build"
 
+CONFIGURE_GCC=(
+    "${CONFIGURE_COMMON[@]}"
+    "--with-cpu=${TARGET_CPU}"
+    "--with-fpu=${TARGET_FPU}"
+    "--with-arch=${TARGET_ARCH}"
+    "--with-float=${TARGET_FLOAT}"
+    "--with-specs=${TARGET_SPECS}"
+    "--enable-poison-system-directories"
+    "--enable-threads=posix"
+    "--enable-languages=c,c++,fortran"
+    "--enable-shared"
+    "--with-gcc-major-version-only"
+    "--enable-linker-build-id"
+    "--enable-__cxa_atexit" # Should be enabled on glibc devices
+    "--libdir=${SYSROOT_PATH}/usr/lib"
+    "--with-gxx-include-dir=${SYSROOT_PATH}/usr/include/c++/${V_GCC/.*/}"
+)
+
+if [ "${WPI_HOST_NAME}" != "Windows" ]; then
+    # Use system zlib when building target code on a *nix enviorment
+    CONFIGURE_GCC+=("--with-system-zlib")
+fi
+
+if [ "${TARGET_DISTRO}" = "roborio" ]; then
+    # Pulled by running gcc -v on target device
+    CONFIGURE_GCC+=(
+        "--disable-libmudflap"
+        "--enable-c99"
+        "--enable-symvers=gnu"
+        "--enable-long-long"
+        "--enable-libstdcxx-pch"
+        "--enable-lto"
+        "--enable-libssp"
+        "--enable-libitm"
+        "--enable-initfini-array"
+        "--with-linker-hash-style=gnu"
+        "--with-gnu-ld"
+        "--with-ppl=no"
+        "--with-cloog=no"
+        "--with-isl=no"
+        "--without-long-double-128"
+    )
+else
+    # Pulled by running gcc -v on target devices
+    CONFIGURE_GCC+=(
+        "--enable-clocal=gnu"
+        "--without-included-gettext"
+        "--enable-libstdcxx-debug"
+        "--enable-libstdcxx-time=yes"
+        "--with-default-libstdcxx-abi=new"
+        "--enable-gnu-unique-object"
+        "--enable-plugin"
+        "--enable-default-pie"
+    )
+    case "${TARGET_PORT}" in
+    amd64) CONFIGURE_GCC+=(
+        "--disable-vtable-verify"
+        "--enable-libmpx"
+    ) ;;
+    armhf) CONFIGURE_GCC+=(
+        "--disable-sjlj-exceptions"
+        "--disable-libitm"
+        "--disable-libquadmath"
+        "--disable-libquadmath-support"
+    ) ;;
+    arm64) CONFIGURE_GCC+=(
+        "--disable-libquadmath"
+        "--disable-libquadmath-support"
+    ) ;;
+    esac
+fi
+
 xpushd "${BUILD_DIR}/gcc-build"
 "$DOWNLOAD_DIR/gcc-${V_GCC}/configure" \
-    "${CONFIGURE_COMMON[@]}" \
-    "--with-cpu=${TARGET_CPU}" \
-    "--with-fpu=${TARGET_FPU}" \
-    "--with-arch=${TARGET_ARCH}" \
-    "--with-float=${TARGET_FLOAT}" \
-    "--with-specs=${TARGET_SPECS}" \
-    "--enable-poison-system-directories" \
-    "--enable-threads=posix" \
-    "--enable-languages=c,c++,fortran" \
-    "--enable-shared" \
-    "--enable-__cxa_atexit" \
-    "--enable-default-pie" \
-    "--disable-libmudflap" \
-    "--with-gcc-major-version-only" \
-    "--libdir=${SYSROOT_PATH}/usr/lib" \
-    "--with-gxx-include-dir=${SYSROOT_PATH}/usr/include/c++/${V_GCC/.*/}" ||
+    "${CONFIGURE_GCC[@]}" ||
     die "gcc configure failed"
 
 make -j"$JOBS" \

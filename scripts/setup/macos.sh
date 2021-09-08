f3950cd9932138e66bcdb9adc990ea895d302da8
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-09-07 22:13:42 -0500

    Simplify macOS SDK deployment setting

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -2,18 +2,9 @@
 
 is-mac || return 0
 
-SDK_PATH="/Library/Developer/CommandLineTools/SDKs"
+MACOSX_DEPLOYMENT_TARGET="${WPI_HOST_SDK_MIN}"
+CFLAGS="${CFLAGS} -fcommon"
+CXXFLAGS="${CXXFLAGS} -fcommon"
 
-ls -l "$SDK_PATH"
-[ -d "$SDK_PATH/MacOSX.sdk" ] || exit
-
-MacOS Flags
-CPPFLAGS="-isysroot $SDK_PATH/MacOSX.sdk"
-CPPFLAGS+=" -I$SDK_PATH/MacOSX.sdk/usr/include/"
-CPPFLAGS+=" -mmacosx-version-min=${WPI_HOST_SDK_MIN}"
-CFLAGS="${CPPFLAGS} -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
-CXXFLAGS="${CPPFLAGS} -fcommon $WPI_HOST_CPP_FLAGS_APPEND"
-
-export CPPFLAGS CFLAGS CXXFLAGS
-unset SDK_PATH
+export CFLAGS CXXFLAGS MACOSX_DEPLOYMENT_TARGET
 

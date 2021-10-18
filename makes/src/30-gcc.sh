8de718a1138179324f0742ec0b9188af0be0b2d9
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2021-10-17 22:41:43 -0500

    Fix build error

--- makes/src/30-gcc.sh
+++ makes/src/30-gcc.sh
@@ -41,8 +41,8 @@ fi
 if [ "${TARGET_DISTRO}" = "roborio" ]; then
     # Pulled by running gcc -v on target device
     CONFIGURE_GCC+=(
-        "--libdir=${SYSROOT_PATH}/usr/lib" \
-        "--with-toolexeclibdir=${SYSROOT_PATH}/usr/lib" \
+        "--libdir=${SYSROOT_PATH}/usr/lib"
+        "--with-toolexeclibdir=${SYSROOT_PATH}/usr/lib"
         "--enable-languages=c,c++,fortran"
         "--disable-libmudflap"
         "--enable-c99"
@@ -64,8 +64,8 @@ else
     # Pulled by running gcc -v on target devices
     CONFIGURE_GCC+=(
         # Debian specific flags
-        "--libdir=${SYSROOT_PATH}/usr/lib/" \
-        "--with-toolexeclibdir=${SYSROOT_PATH}/lib/${TARGET_TUPLE}" \
+        "--libdir=${SYSROOT_PATH}/usr/lib/"
+        "--with-toolexeclibdir=${SYSROOT_PATH}/lib/${TARGET_TUPLE}"
         "--enable-languages=c,c++"
         "--enable-clocal=gnu"
         "--without-included-gettext"
@@ -109,8 +109,10 @@ function _make_multi() {
             DESTDIR="${BUILD_DIR}/gcc-install" \
             "${@}" || die "$msg"
     }
-    _make "$1" "all-$2"
-    _make_installer "$1" "install-$2"
+    process_background "Building GCC '$2'" \
+        _make "$1" "all-$2"
+    process_background "Installing GCC '$2'" \
+        _make_installer "$1" "install-$2"
 }
 
 xpushd "${BUILD_DIR}/gcc-build"
@@ -118,34 +120,31 @@ process_background "Configuring GCC" \
     "$DOWNLOAD_DIR/gcc-${V_GCC}/configure" \
     "${CONFIGURE_GCC[@]}" ||
     die "gcc configure failed"
+
 # Build the core compiler without libraries
-process_background "Building GCC frontend" \
-    _make_multi "gcc build failed" gcc
+_make_multi "gcc build failed" gcc
 
-if [ "${CANADIAN_STAGE_ONE}" = "true" ]; then
-    # We don't need support libraries for the first stage
-    exit 0
-fi
+# libgcc is a complicated beast. Its easier to just build it ourselves
+TASKS=(target-libgcc)
 
-TASKS=()
-case "${TARGET_DISTRO}" in
-roborio)
-    TASKS+=(
-        target-libgfortran
-        target-libsanitizer
-    )
-    if [ "${TARGET_LIB_REBUILD}" = "true" ]; then
+if [ "${CANADIAN_STAGE_ONE}" != "true" ]; then
+    case "${TARGET_DISTRO}" in
+    roborio)
         TASKS+=(
-            target-libgcc
-            target-libatomic
-            target-libstdc++-v3
+            target-libgfortran
+            target-libsanitizer
         )
-    fi
-    ;;
-*) ;; # No current need to build support libraries for debian targets
-esac
+        if [ "${TARGET_LIB_REBUILD}" = "true" ]; then
+            TASKS+=(
+                target-libatomic
+                target-libstdc++-v3
+            )
+        fi
+        ;;
+    *) ;; # No current need to build support libraries for debian targets
+    esac
+fi
 for task in "${TASKS[@]}"; do
-    process_background "Building GCC $task" \
-        _make_multi "GCC $task" "$task"
+    _make_multi "GCC $task" "$task"
 done
 xpopd

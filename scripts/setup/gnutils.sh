498ce238cbb90481a7503658a30220f87fc7c334
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2021-10-19 14:55:42 -0500

    Further tool switching on macOS

--- scripts/setup/gnutils.sh
+++ scripts/setup/gnutils.sh
@@ -4,6 +4,7 @@ if is-mac; then
     BREW_PREFIX="$(brew --prefix)"
     GNU_PROJECTS=(coreutils binutils gnu-tar make)
     for project in "${GNU_PROJECTS[@]}"; do
+        PATH="${BREW_PREFIX}/opt/${project}/bin:$PATH"
         PATH="${BREW_PREFIX}/opt/${project}/libexec/gnubin:$PATH"
     done
     unset BREW_PREFIX

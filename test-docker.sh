eee41c99feaa036a2bc81cf67b49e17e67fa371b
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2021-10-19 18:57:45 -0500

    Remove singleshell

--- test-docker.sh
+++ test-docker.sh
@@ -1,13 +1,20 @@
 #! /usr/bin/env bash
 
-TAG="$1" bash res/build.sh
+set -e
 
+DOCKER_ARG=""
 if [ "$CI" != true ]; then
     DOCKER_ARG="-it"
 fi
 
+PLATFORM="linux/amd64"
+if [[ "$1" =~ "linux_i686" ]]; then
+    PLATFORM="linux/386"
+fi
+
 docker run \
     --rm $DOCKER_ARG -v "${PWD}:/work" \
+    --platform "${PLATFORM}" \
     -e CI="$CI" \
-    "wpilib/toolchain-builder:$1" \
-    bash -c "cd /work; ./test.sh '$2' '$3' '$4'"
+    "ghcr.io/ryanhir/toolchain-builder:latest" \
+    bash -c "cd /work; ./test.sh '$1' '$2' '$3'"
--- test.sh
+++ test.sh
@@ -11,15 +11,15 @@ source "$ROOT_DIR/scripts/setup.sh"
 MAKE="make -C ${ROOT_DIR}/makes/ M=${BUILD_DIR}"
 
 ARCHIVE_NAME=$(${MAKE} --no-print-directory print-pkg)
-if [ ! -f "$ROOT_DIR/$ARCHIVE_NAME" ]; then
-    echo "[ERR] $ARCHIVE_NAME not found in base of project"
+if [ ! -f "$ROOT_DIR/output/$ARCHIVE_NAME" ]; then
+    echo "[ERROR] $ARCHIVE_NAME not found in output dir"
     exit 1
 fi
 
 pushd /tmp/
 mkdir -p toolchain
 pushd toolchain
-tar xf "$ROOT_DIR/$ARCHIVE_NAME"
+tar xf "$ROOT_DIR/output/$ARCHIVE_NAME"
 cd "${TOOLCHAIN_NAME}"
 cat << EOF > hello.c
 #include <stdio.h>

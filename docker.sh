f091f69f9e45b38b04316eeac85f154ebad9dfaa
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-03-27 16:13:59 -0500

    Create unified container image launcher

---
+++ docker.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/env bash
+
+CONTAINER_IMAGE="ghcr.io/ryanhir/toolchain-builder"
+CONTAINER_VERSION="bionic"
+
+DOCKER_ARGS=(
+    --rm
+    -v "${PWD}:/work"
+    -w "/work"
+    -e CI="$CI"
+    --platform "linux/amd64")
+
+die() {
+    echo "ERROR:" "$@" >&2
+    exit 1
+}
+
+if [ "$CI" != "true" ]; then
+    DOCKER_ARGS+=("-it")
+fi
+
+# Check if system is Linux
+if [ "$(uname)" != "Linux" ]; then
+    die "This script is only for Linux runners"
+fi
+
+# Check if docker is installed
+if ! command -v docker >/dev/null 2>&1; then
+    die "Docker is not installed"
+fi
+
+# Check if docker daemon is running
+if ! docker info >/dev/null 2>&1; then
+    die "Docker daemon is not running"
+fi
+
+# Check if working directory exists
+if [ ! -d "${PWD}" ]; then
+    # This should never happen
+    die "Working directory does not exist"
+fi
+
+docker run \
+    "${DOCKER_ARGS[@]}" \
+    "${CONTAINER_IMAGE}:${CONTAINER_VERSION}" \
+    bash -c "$*"

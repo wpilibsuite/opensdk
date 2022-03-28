a2a7a238ca20e3d59741737f3e5c48fd7638c52a
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-03-28 14:18:05 -0500

    Canadian helpers

--- makes/src/common.sh
+++ makes/src/common.sh
@@ -52,6 +52,93 @@ function process_background() {
     return "$retval"
 }
 
+env_exists() {
+    local env_var="$1"
+    if [ -z "${!env_var}" ]; then
+        die "$env_var is not set"
+    else
+        return 0
+    fi
+}
+
+configure_host_vars() {
+    local xcode_arch_flag
+    local xcode_sdk_flag
+    local xcrun_find
+    # xcode_sdk_flag="-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
+    xcrun_find="xcrun --sdk macosx -f"
+
+    env_exists WPI_HOST_NAME
+    env_exists WPI_HOST_TUPLE
+    env_exists HOST_TUPLE
+
+    if [ "$WPI_HOST_NAME" = "Mac" ]; then
+        case "${WPI_HOST_TUPLE}" in
+        arm64* | aarch64*) xcode_arch_flag="-arch arm64" ;;
+        x86_64*) xcode_arch_flag="-arch x86_64" ;;
+        *) die "Unsupported Canadian config" ;;
+        esac
+
+        export AR="$($xcrun_find ar)"
+        export AS="$($xcrun_find as) $xcode_arch_flag"
+        export LD="$($xcrun_find ld) $xcode_arch_flag $xcode_sdk_flag"
+        export NM="$($xcrun_find nm) $xcode_arch_flag"
+        export STRIP="$($xcrun_find strip) $xcode_arch_flag"
+        export RANLIB="$($xcrun_find ranlib)"
+        export OBJDUMP="$($xcrun_find objdump)"
+        export CC="$($xcrun_find gcc) $xcode_arch_flag $xcode_sdk_flag"
+        export CXX="$($xcrun_find g++) $xcode_arch_flag $xcode_sdk_flag"
+    else
+        export AR="/usr/bin/${HOST_TUPLE}-ar"
+        export AS="/usr/bin/${HOST_TUPLE}-as"
+        export LD="/usr/bin/${HOST_TUPLE}-ld"
+        export NM="/usr/bin/${HOST_TUPLE}-nm"
+        export RANLIB="/usr/bin/${HOST_TUPLE}-ranlib"
+        export STRIP="/usr/bin/${HOST_TUPLE}-strip"
+        export OBJCOPY="/usr/bin/${HOST_TUPLE}-objcopy"
+        export OBJDUMP="/usr/bin/${HOST_TUPLE}-objdump"
+        export READELF="/usr/bin/${HOST_TUPLE}-readelf"
+        export CC="/usr/bin/${HOST_TUPLE}-gcc"
+        export CXX="/usr/bin/${HOST_TUPLE}-g++"
+    fi
+}
+
+configure_target_vars() {
+    env_exists TARGET_TUPLE
+
+    define_target_export() {
+        local var="$1"
+        local tool="$2"
+        if [ "${!var}" ]; then
+            die "$var is already set"
+        else
+            export "${var}_FOR_TARGET"="/opt/frc/bin/${TARGET_TUPLE}-$tool"
+        fi
+    }
+
+    define_target_export AR ar
+    define_target_export AS as
+    define_target_export LD ld
+    define_target_export NM nm
+    define_target_export RANLIB ranlib
+    define_target_export STRIP strip
+    define_target_export OBJCOPY objcopy
+    define_target_export OBJDUMP objdump
+    define_target_export READELF readelf
+    define_target_export CC gcc
+    define_target_export CXX g++
+    define_target_export GCC gcc
+    define_target_export GFORTRAN gfortran
+}
+
+check_if_canandian_stage_one_succeded() {
+    env_exists TARGET_TUPLE
+    if ! [[ -x "/opt/frc/bin/${TARGET_TUPLE}-gcc" ]]; then
+        echo "[DEBUG]: Cannot find ${TARGET_TUPLE}-gcc in /opt/frc/bin"
+        die "Stage 1 Canadian toolchain not found in expected location"
+    fi
+}
+
 # If these fail, then others are bad aswell
 [ "${V_BIN:-fail}" != fail ] || die "V_BIN"
 [ "${V_GDB:-fail}" != fail ] || die "V_GDB"

12f6db143af5e2aa9cdece85edf9784546503950
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-04-15 13:09:22 -0700

    Remove unneeded macOS compiler flags

--- scripts/setup/flags.sh
+++ scripts/setup/flags.sh
@@ -1,13 +1,5 @@
 #! /usr/bin/env bash
 
-# Core flags
-CFLAGS="$CFLAGS -Os -g0"
-CXXFLAGS="$CXXFLAGS -Os -g0"
-if [ "$WPI_HOST_NAME" = "Mac" ]; then
-    # Clang has a lower bracket depth than what GCC may have
-    CFLAGS+=" -fbracket-depth=512 -fcommon"
-    CXXFLAGS+=" -fbracket-depth=512 -fcommon"
-fi
 if [ "$WPI_HOST_NAME" = "Windows" ]; then
     CFLAGS="$CFLAGS -static-libgcc -static-libstdc++"
     CXXFLAGS="$CXXFLAGS -static-libgcc -static-libstdc++"

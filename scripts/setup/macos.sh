62cdbed790a8338de091baf0e7215f6f77d750b5
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-04-15 09:45:57 -0700

    Remove darwin version from GNU triple

--- scripts/setup/macos.sh
+++ scripts/setup/macos.sh
@@ -4,8 +4,3 @@
 # of macOS to target. For M1, Xcode will default to
 # version 11.0
 export MACOSX_DEPLOYMENT_TARGET=10.14
-
-if [[ "$(cc -dumpmachine)" =~ $WPI_HOST_TUPLE ]]; then
-    # Native build
-    export WPI_HOST_TUPLE="$(cc -dumpmachine)"
-fi

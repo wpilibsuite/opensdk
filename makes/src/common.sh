c450c666a994f8de8e5f207869f061556cbdb77e
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-04-29 12:10:27 -0500

    Change libexec location back to default

--- makes/src/common.sh
+++ makes/src/common.sh
@@ -60,7 +60,6 @@ CONFIGURE_COMMON_LITE=(
 CONFIGURE_COMMON=(
     "${CONFIGURE_COMMON_LITE[@]}"
     "--target=${TARGET_TUPLE}"
-    "--libexecdir=${WPI_HOST_PREFIX}/${TARGET_TUPLE}/libexec"
     "--with-sysroot=${SYSROOT_PATH}"
     "--libdir=${SYSROOT_PATH}/usr/lib"
     "--with-toolexeclibdir=${SYSROOT_PATH}/usr/lib"

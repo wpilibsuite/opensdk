73378a82190d5fd1ca31f91ba83236e4e1dcc016
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-05-28 13:56:59 -0400

    Disable GMP assembly for Intel Macs

--- makes/src/21-gmp.sh
+++ makes/src/21-gmp.sh
@@ -31,10 +31,15 @@ fi
 rm -rf "${BUILD_DIR}/gmp-build" "${BUILD_DIR}/gmp-install"
 mkdir "${BUILD_DIR}/gmp-build"
 
+if [ "${WPI_HOST_TUPLE}" = "x86_64-apple-darwin" ]; then
+    EXTRA_FLAGS="--disable-assembly"
+fi
+
 xcd "${BUILD_DIR}/gmp-build"
 process_background "Configuring GMP" \
     "$DOWNLOAD_DIR/gmp-${V_GMP}/configure" \
     "${CONFIGURE_COMMON_LITE[@]}" \
+    $EXTRA_FLAGS \
     --enable-static \
     --disable-shared ||
     die "GMP configure failed"

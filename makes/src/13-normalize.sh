164bcaa0761aa2244a430c667bcbeeea544ada4b
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-08-10 16:59:29 -0400

    Fix missing libm archive and fail if move errors (#92)

--- makes/src/13-normalize.sh
+++ makes/src/13-normalize.sh
@@ -37,25 +37,25 @@ xcd "${BUILD_DIR}/sysroot-install/${TARGET_TUPLE}/sysroot"
 # relocated for the toolchain to work.
 relocate_core_libraries() {
     mkdir -p $2
-    mv $1/libm-2.42.a $2/libm-2.42.a
-    mv $1/libmvec.a $2/libmvec.a
-    mv $1/libc_nonshared.a $2/libc_nonshared.a
+    mv $1/libm-2.*.a $2/ || die "Couldn't move libm archive"
+    mv $1/libmvec.a $2/ || die "Couldn't move libmvec archive"
+    mv $1/libc_nonshared.a $2/ || die "Couldn't move libc_nonshared archive"
 }
 
 if [ "${TARGET_DISTRO}" = "systemcore" ]; then
     # Required for <bits/c++config.h> to be in the right spot
-    mv "usr/include/c++/${V_GCC/.*/}/aarch64-buildroot-linux-gnu/bits"/* "usr/include/c++/14/bits/"
-    mv "usr/include/c++/${V_GCC/.*/}/aarch64-buildroot-linux-gnu/ext"/* "usr/include/c++/14/ext/"
+    mv "usr/include/c++/${V_GCC/.*/}/aarch64-buildroot-linux-gnu/bits"/* "usr/include/c++/14/bits/" || die "Couldn't move C++ bits/ headers"
+    mv "usr/include/c++/${V_GCC/.*/}/aarch64-buildroot-linux-gnu/ext"/* "usr/include/c++/14/ext/" || die "Couldn't move C++ ext/ headers"
     # We want to minimize library relocations, so it would be good to
     # move everything one time to a place that will require the least
     # number of relocations afterwards. Of the libraries in the
     # linker scripts, /lib64 and /usr/lib64 show up the most
-    # (3 times). We will choose move the majority of libraries to
+    # (3 times). We will choose to move the majority of libraries to
     # /lib64 due to the path being shorter, which may help with
     # Windows path length later.
     relocate_core_libraries usr/lib usr/lib64
     mkdir -p lib
-    mv usr/lib/ld-linux-aarch64.so.1 lib/ld-linux-aarch64.so.1
+    mv usr/lib/ld-linux-aarch64.so.1 lib/ || die "Couldn't move ld-linux-aarch64.so.1"
     # Relocate everything
     mv usr/lib lib64
     # Move gcc/aarch64-linux-gnu directory back
